993b745b49aa990274ce8c2fbf1ebe6d2064c606
Grey601 <grev.gr@hotmail.com>
2022-10-11 19:04:55 +0300

    Code reformatted

--- src/test/java/ru/netology/repository/RepositoryTest.java
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -9,11 +9,11 @@ import ru.netology.domain.Smartphone;
 public class RepositoryTest {
 
     Repository repo = new Repository();
-    Book book1 = new Book(1, "Война и Мир", 1600, "Толстой" );
-    Book book2 = new Book(2, "Скотный двор", 1000, "Оруэлл" );
-    Book book3 = new Book(3, "1984", 2900, "Оруэлл" );
-    Smartphone phone1 = new Smartphone(4, "iPhone13", 60_000, "Apple" );
-    Smartphone phone2 = new Smartphone(5, "Samsung Galaxy S22", 55_000, "Samsung" );
+    Book book1 = new Book(1, "Война и Мир", 1600, "Толстой");
+    Book book2 = new Book(2, "Скотный двор", 1000, "Оруэлл");
+    Book book3 = new Book(3, "1984", 2900, "Оруэлл");
+    Smartphone phone1 = new Smartphone(4, "iPhone13", 60_000, "Apple");
+    Smartphone phone2 = new Smartphone(5, "Samsung Galaxy S22", 55_000, "Samsung");
 
     @Test
     public void shouldSaveTest() {
@@ -54,4 +54,5 @@ public class RepositoryTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
 }
